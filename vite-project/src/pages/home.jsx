21e2392bb58d4d32e1117ad8164467bd3bb2ac52
Angelin <z5438386@student.unsw.edu.au>
2024-08-24 13:11:54 +1000

    Added some css into the landing page

--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -2,10 +2,15 @@ import { useNavigate } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="home">
-      <p>HOME!</p>
+    <div className="innerContainer">
+      <h1>ResuManiac</h1>
+      <h3 className="subHeadings">ACE YOUR INTERVIEW</h3>
       {/* Temporary button, remove later */}
-      <button onClick={() => useNavigate('resume')}>to resume page!</button>
+
+      <div className="inputContainer">
+        
+        <button onClick={() => useNavigate('resume')}>to resume page!</button>
+      </div>
     </div>
   )
 }
