a237df27d95002122452b704ce3485cccdaf9fa6
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2024-08-24 13:29:39 +1000

    updated css

--- vite-project/src/pages/resume.jsx
+++ vite-project/src/pages/resume.jsx
@@ -3,7 +3,17 @@ function Resume() {
 
   return (
     <div className="resume">
-      <p>RESUME!</p>
+      <div className="container">
+        <p>Resume</p>
+
+        <div className="image">
+          {/* image */}
+        </div>
+        <div className="resume-container">
+          {/* CV */}
+          Im a container
+        </div>
+      </div>
     </div>
   )
 }
