9b309d8da127779d1f64bf8ccfdda4d9e2605518
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2024-08-24 18:25:54 +1000

    removed unnecessary variables

--- vite-project/src/pages/takePicturePage.jsx
+++ vite-project/src/pages/takePicturePage.jsx
@@ -3,26 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import WebcamCapture from '../components/Webcam';
 
 function TakePic() {
-  const imgID = document.getElementById('imgID');
-  const imgForm = document.getElementById('imgForm');
   const navigate = useNavigate();
-  const upload = () => {
-    imgForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      console.log("submit img");
-      fetch(imgForm.action, {
-        method: 'POST',
-        body: new FormData(imgForm)
-      })
-    });
-
-    imgID.addEventListener('change', (event) => {
-      if (event?.target.files && event.target.files[0]) {
-        imgID.src = URL.createObjectURL(event.target.files[0]);
-        imgID.load()
-      }
-    });
-  }
 
   return (
     <div className="innerContainer">
