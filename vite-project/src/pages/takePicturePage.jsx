a1f74d197713040fe2e7b4bc0a9ec937d2b39a04
Winn Z <winnwyz@gmail.com>
2024-08-24 18:59:58 +1000

    Change the textbox style

--- vite-project/src/pages/takePicturePage.jsx
+++ vite-project/src/pages/takePicturePage.jsx
@@ -33,9 +33,14 @@ function TakePic() {
         <WebcamCapture />
       </div>
 
-      <div className="biggerContainer">
-        <div className="inputContainer"></div>
-      </div>
+
+      <form action="" method="get">
+          <textarea
+            className="inputContainer"
+            placeholder='Put the job position you want to apply for, e.g. Software Engineer'
+            rows="4"
+          />
+      </form>
       <button className="take-pic-button" onClick={() => navigate('/')}>Upload file</button>
     </div>
   )
