a89fd296998193d0c93c2f305dcb0e7363958c4b
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2024-08-25 14:04:45 +1000

    pulled

--- vite-project/src/pages/resume.jsx
+++ vite-project/src/pages/resume.jsx
@@ -1,40 +1,24 @@
 import OpenAIbot from "../OpenAIbot";
 import TextToSpeech from "../TextToSpeech";
 import Mouth from '../assets/mouth.gif'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Resume() {
   const location = useLocation();
-
-  const label = location.state;
-  // const [resumeData, setResumeData] = useState(null);
-  // useEffect(() => {
-  //   const fetchResumeData = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:3000/api/resume');
-  //       const data = await response.json();
-  //       setResumeData(data);
-  //     } catch (error) {
-  //       console.error('Error fetching resume data:', error);
-  //     }
-  //   };
-
-  //   fetchResumeData();
-  // }, []);
-
+  const { label, img } = location.state;
+  const navigate = useNavigate();
 
   return (
     <div className="resume">
-      <p>RESUME!</p>
       <TextToSpeech />
       <div className="container">
         <button onClick={() => navigate('../')} className="back-button"></button>
-        <div className="image">
+        {label && <div className="image" style={{ backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat' }}>
           <img className="mouth-pic" src={Mouth} />
-        </div>
+        </div>}
         <div className="resume-container">
           {/* the CV */}
-          <OpenAIbot label={label}/>
+          <OpenAIbot label={label} />
         </div>
       </div>
     </div>
